29c29216462dfcc39ea467c8b34f0395f2c089df
Jan Fontanilla <janfontanilla12@gmail.com>
2025-06-26 18:28:23 -0400

    Fix missing Supabase environment variables

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,15 @@ module.exports = function (api) {
       ['@babel/plugin-transform-modules-commonjs', {
         allowTopLevelThis: true
       }],
+      // Add react-native-dotenv plugin to load environment variables
+      ['module:react-native-dotenv', {
+        'moduleName': '@env',
+        'path': '.env',
+        'blocklist': null,
+        'allowlist': null,
+        'safe': false,
+        'allowUndefined': true
+      }],
       // Add reanimated plugin
       'react-native-reanimated/plugin',
     ],
