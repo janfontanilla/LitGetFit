9d7bc895dc20bc9e2276b0e3f5aed9e3b4258f9f
Jan Fontanilla <janfontanilla12@gmail.com>
2025-06-27 18:50:42 -0400

    Fix Metro Bundler Errors

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,19 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Add resolver options to handle the React Native SVG issue
+config.resolver = {
+  ...config.resolver,
+  sourceExts: [...config.resolver.sourceExts, 'mjs'],
+  extraNodeModules: {
+    ...config.resolver.extraNodeModules,
+  }
+};
+
+// Ensure we're using the correct transformer
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('metro-react-native-babel-transformer'),
+};
+
 module.exports = config;
